6f008ab54a3213a92a8fe64aba444e4df5af6a38
Adrian Rangel <adrian.rangel@gmail.com>
2025-10-12 14:47:04 -0600

    automatic release on new version

--- scripts/release-github.sh
+++ scripts/release-github.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-RELEASE_NOTES="${1}"
+# Allow passing release notes either as the first argument or via the RELEASE_NOTES env var.
+RELEASE_NOTES="${1:-${RELEASE_NOTES}}"
 export RELEASE_NOTES
 
 echo "Release notes set to: $RELEASE_NOTES"
